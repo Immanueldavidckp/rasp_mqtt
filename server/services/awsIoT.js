ea58aa3581e17e924a1c32985fb7b47cf65e1078
Immanuel David <immanueldavidckp@gmail.com>
2025-10-05 18:32:57 +0530

    update

--- server/services/awsIoT.js
+++ server/services/awsIoT.js
@@ -26,7 +26,7 @@ const initializeAWSIoT = (io) => {
     if (process.env.AWS_IOT_ENDPOINT && process.env.AWS_IOT_THING_NAME) {
       iotDevice = AWSIoTData.device({
         keyPath: './aws_certificate/763d836c18927e59bebd8cf25f966fe43b7ad7e6a1ed469efec374b2d1fb3b56-private.pem.key',
-        certPath: './aws_certificate/763d836c18927e59bebd8cf25f966fe43b7ad7e6a1ed469efec374b2d1fb3b56-certificate.pem.crt',
+        certPath: './aws_certificate/a73d836c18927e59bebd8cf25f966fe43b7ad7e6a1ed469efec374b2d1fb3b56-certificate.pem.crt',
         caPath: './aws_certificate/AmazonRootCA1.pem',
         clientId: process.env.AWS_IOT_THING_NAME || 'webdashboard-client',
         host: process.env.AWS_IOT_ENDPOINT,
